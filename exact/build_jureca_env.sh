e02a06fea0781646bb2fb189d4592654e549c427
aach1 <m.aach@fz-juelich.de>
2024-12-10 16:33:00 +0100

    python env

--- exact/build_jureca_env.sh
+++ exact/build_jureca_env.sh
@@ -6,4 +6,4 @@ source nano_gpt_env/bin/activate
   
 pip3 install --no-cache-dir torch==2.1.0+cu121 torchvision --index-url https://download.pytorch.org/whl/cu121
 
-pip3 install numpy transformers datasets tiktoken wandb tqdm
+pip3 install ray numpy transformers datasets tiktoken wandb tqdm
