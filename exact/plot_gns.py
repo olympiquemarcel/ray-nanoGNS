1fea7667726cb00538170ab86aa2ed672cad08f5
aach1 <m.aach@fz-juelich.de>
2024-12-11 10:34:10 +0100

    first runs results

--- exact/plot_gns.py
+++ exact/plot_gns.py
@@ -4,19 +4,49 @@ import matplotlib.pyplot as plt
 # Initialize lists to store the data
 steps = []
 ddp_gns_values = []
+val_losses = []
 
-# Read the CSV file
-with open('out/test/log.csv', 'r') as file:
-    reader = csv.DictReader(file)
-    for row in reader:
-        steps.append(int(row['step']))
-        ddp_gns_values.append(float(row['ddp/gns']))
+widths = [256, 512, 1024]
 
-# Plot the data
-plt.plot(steps, ddp_gns_values, label='ddp/gns')
+plt.figure(1)
+for width in widths:
+    # Read the CSV file
+    with open(f'out/width_{width}_lr_0.00390625/log.csv', 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            steps.append(int(row['step']))
+            ddp_gns_values.append(float(row['ddp/gns']))
+
+    # Plot the data
+    plt.plot(steps, ddp_gns_values, label=f'width={width}')
+    steps = []
+    ddp_gns_values = []
+
+# # Plot the data
 plt.xlabel('Step')
 plt.ylabel('ddp/gns')
 plt.title('Step vs ddp/gns')
 plt.legend()
-plt.savefig("gns_plot.png", dpi = 300)
-plt.show()
+plt.savefig("gns_plot_widths.png", dpi = 300)
+
+plt.figure(2)
+for width in widths:
+    # Read the CSV file
+    with open(f'out/width_{width}_lr_0.00390625/log.csv', 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            steps.append(int(row['step']))
+            val_losses.append(float(row['train/lossf']))
+
+    # Plot the data
+    plt.plot(steps, val_losses, label=f'width={width}')
+    steps = []
+    val_losses = []
+
+# # Plot the data
+plt.xlabel('Step')
+plt.ylabel('train/loss')
+plt.title('Step vs val/loss')
+plt.legend()
+plt.savefig("train_loss_widths.png", dpi = 300)
+
